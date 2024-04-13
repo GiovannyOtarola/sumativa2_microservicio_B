199a6c45cdbf651da57f14371e2a58047f899f2a
Giovanny Otarola <giovannyxdd@gmail.com>
2024-04-13 11:26:31 -0400

    Create ConsultaMedicaRepository.java

---
+++ demo/src/main/java/com/example/demo/repository/ConsultaMedicaRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.ConsultaMedica;
+
+public interface ConsultaMedicaRepository extends JpaRepository<ConsultaMedica, Long>{
+
+    
+
+    
+}
