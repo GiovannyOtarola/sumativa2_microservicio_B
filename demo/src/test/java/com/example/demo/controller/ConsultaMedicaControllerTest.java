c4fdae287df9a34bcd42df72ef659ac368a7058d
Giovanny Otarola <giovannyxdd@gmail.com>
2024-04-30 19:35:40 -0400

    creación de mas Test

--- demo/src/test/java/com/example/demo/controller/ConsultaMedicaControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/ConsultaMedicaControllerTest.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 
 import java.util.Optional;
-
+import java.util.List;
+import java.util.Arrays;
 import org.hamcrest.Matchers;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -45,6 +50,56 @@ public class ConsultaMedicaControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.diagnostico", Matchers.is("le duele la cabeza")))
             .andExpect(MockMvcResultMatchers.jsonPath("$.fecha", Matchers.is("2024-04-11")))
             .andExpect(MockMvcResultMatchers.jsonPath("$.pacienteid", Matchers.is(1)));
+    }
+
+    @Test
+    public void obtenerTodosTest() throws Exception{
+        ConsultaMedica consultaMedica1 = new ConsultaMedica();
+        consultaMedica1.setMotivoConsulta("Dolor cabeza");
+        consultaMedica1.setDiagnostico("le duele la cabeza");
+        consultaMedica1.setFecha(LocalDate.of(2024, 4, 11));
+        consultaMedica1.setPacienteid(1L);
+        consultaMedica1.setId(1L);
+
+        ConsultaMedica consultaMedica2 = new ConsultaMedica();
+        consultaMedica2.setMotivoConsulta("Dolor espalda");
+        consultaMedica2.setDiagnostico("le duele la espalda");
+        consultaMedica2.setFecha(LocalDate.of(2024, 4, 28));
+        consultaMedica2.setPacienteid(2L);
+        consultaMedica2.setId(2L);
+
+        List<ConsultaMedica> consultasMedicas =  Arrays.asList(consultaMedica1,consultaMedica2);
+        
+        when(consultaMedicaServiceMock.getAllConsultasMedicas()).thenReturn(consultasMedicas);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/ConsultasMedicas"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList", Matchers.hasSize(2)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[0].motivoConsulta", Matchers.is("Dolor cabeza")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[0].diagnostico", Matchers.is("le duele la cabeza")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[0].fecha", Matchers.is("2024-04-11")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[0].pacienteid", Matchers.is(1)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[0]._links.self.href", Matchers.is("http://localhost/ConsultasMedicas/1")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[1].motivoConsulta", Matchers.is("Dolor espalda")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[1].diagnostico", Matchers.is("le duele la espalda")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[1].fecha", Matchers.is("2024-04-28")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[1].pacienteid", Matchers.is(2)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.consultaMedicaList[1]._links.self.href", Matchers.is("http://localhost/ConsultasMedicas/2")));
+    }
+
+    @Test
+    public void deleteConsultaMedicaTest() throws Exception {
+        // ID del usuario a eliminar
+        Long consultaMedicaId = 1L;
+
+        // Simular la llamada al servicio deleteUsuario
+        doNothing().when(consultaMedicaServiceMock).deleteConsultaMedica(consultaMedicaId);
+
+        // Realizar la solicitud DELETE al endpoint /Usuarios/{id}
+        mockMvc.perform(MockMvcRequestBuilders.delete("/ConsultasMedicas/{id}", consultaMedicaId))
+            .andExpect(MockMvcResultMatchers.status().isOk()); // Verificar el código de estado
+
+        // Verificar que el método deleteUsuario del servicio se llamó con el ID correcto
+        verify(consultaMedicaServiceMock, times(1)).deleteConsultaMedica(consultaMedicaId);
     }
 
 }
--- demo/src/test/java/com/example/demo/repository/ConsultaMedicaRepositoryTest.java
+++ demo/src/test/java/com/example/demo/repository/ConsultaMedicaRepositoryTest.java
@@ -3,7 +3,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
-
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -36,5 +37,41 @@ public class ConsultaMedicaRepositoryTest {
         assertEquals(1L, resultado.getId());
 
     }
+
+    @Test
+    public void obtenerConsultaMedicaPorIdTest(){
+
+       //ConsultaMedica 1 guardado en la base de datos
+       //motivoConsulta: "Fiebre y tos"
+       //Diagnostico: "Gripe común"
+       //Fecha: 11/04/2024
+       //PacienteId: 1
+
+        ConsultaMedica resultado = consultaMedicaRepository.findById(1L).get();
+
+        assertNotNull(resultado.getId());
+        assertEquals("Fiebre y tos", resultado.getMotivoConsulta());
+        assertEquals("Gripe común", resultado.getDiagnostico());
+        assertEquals(LocalDate.of(2024, 4, 11), resultado.getFecha());
+        assertEquals(1L, resultado.getPacienteid());
+        assertEquals(1L, resultado.getId());
+
+    }
     
+    //verificar si el metodo findById funciona correctamente
+    @Test
+    public void buscarConsultaMedicaPorIdTest(){
+        ConsultaMedica consultaMedica = consultaMedicaRepository.findById(1L).get();
+
+        Assertions.assertThat(consultaMedica.getId()).isEqualTo(1L);
+    }
+
+     //verifica si el metodo findAll funciona correctamente y que la base de datos no esta vacia
+     @Test
+     public void findAllConsultasMedicasTest(){
+ 
+         List<ConsultaMedica> consultasMedicas =  consultaMedicaRepository.findAll();
+ 
+         Assertions.assertThat(consultasMedicas.size()).isGreaterThan(0);
+     }
 }
